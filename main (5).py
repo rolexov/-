0bd702df05a06aca1aeedfb0678662705fb12947
rolexov <125002034+rolexov@users.noreply.github.com>
2023-02-14 12:07:17 +0300

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,15 @@
+tseloe = 10
+drobnoe = int(8.4)
+stroka = "No"
+
+big_netseloe = tseloe * 3.5
+drobnoe = drobnoe - 1
+print (tseloe // drobnoe)
+print (big_netseloe // drobnoe)
+
+stroka=("NoNo_YesYesYes")
+
+print (tseloe)
+print(drobnoe)
+print(big_netseloe)
+print(stroka)
